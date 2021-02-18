a92b7f428530d325709d6f65cdc46bd20ca5ce0a
your name <your email>
2021-02-18 12:55:29 +0800

    .

---
+++ src/test/java/cn/onekit/cloud/nut5gsdk/Nut5GsdkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.onekit.cloud.nut5gsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nut5GsdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
