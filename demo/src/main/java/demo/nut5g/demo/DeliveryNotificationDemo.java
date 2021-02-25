1e9c280fd11f6bd17daa8270c9b7e756ec12b80e
1093357043 <73806828+1093357043@users.noreply.github.com>
2021-02-25 17:04:24 +0800

    .

--- demo/src/main/java/demo/nut5g/demo/DeliveryNotificationDemo.java
+++ demo/src/main/java/demo/nut5g/demo/DeliveryNotificationDemo.java
@@ -2,20 +2,26 @@ package demo.nut5g.demo;
 
 import cn.onekit.cloud.nut5g.notification.DeliveryNotification;
 import cn.onekit.cloud.nut5g.notification.request.StatusNotification;
+import cn.onekit.thekit.FileDB;
+import cn.onekit.thekit.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/nut5g/deliveryNotification/sip:2021020501@botplatform.rcs.chinaunicom.cn")
 public class DeliveryNotificationDemo {
     @RequestMapping(value = "/status",method = RequestMethod.POST)
-    public StatusNotification status(
+    public void status(
             HttpServletRequest request
     ) throws Exception {
         //String accessToken = FileDB.get("demo","accessToken").value;
-        return DeliveryNotification.status(request, DemoApplication.accessToken);
+        StatusNotification data =  DeliveryNotification.status(request, DemoApplication.accessToken);
+        String json = JSON.object2string(data);
+        System.out.println(json);
+        FileDB.set("status",new Date().toString(),json);
     }
 }
--- demo/src/main/java/demo/nut5g/demo/MessageNotificationDemo.java
+++ demo/src/main/java/demo/nut5g/demo/MessageNotificationDemo.java
@@ -4,23 +4,27 @@ import cn.onekit.cloud.nut5g.notification.MessageNotification;
 import cn.onekit.cloud.nut5g.notification.NotifyInfoNotification;
 import cn.onekit.cloud.nut5g.notification.request.ReceivemessageNotification;
 import cn.onekit.thekit.FileDB;
+import cn.onekit.thekit.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/nut5g/messageNotification/sip:2021020501@botplatform.rcs.chinaunicom.cn")
 public class MessageNotificationDemo {
 
-    @RequestMapping(value = "/messages",method = RequestMethod.POST)
-    public ReceivemessageNotification recievemessages(
+    @RequestMapping(value = "/messages", method = RequestMethod.POST)
+    public void recievemessages(
             HttpServletRequest request
     ) throws Exception {
         //String accessToken = FileDB.get("demo","accessToken").value;
-        return MessageNotification.receivemessage(request, DemoApplication.accessToken);
+        ReceivemessageNotification data = MessageNotification.receivemessage(request, DemoApplication.accessToken);
+        String json = JSON.object2string(data);
+        System.out.println(json);
+        FileDB.set("recievemessages",new Date().toString(),json);
     }
-
 }
